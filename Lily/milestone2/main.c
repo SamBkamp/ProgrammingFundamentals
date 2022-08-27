61be63108a8220e7d163976a5e16cf88a39b7e4d
Sam Bonnekamp <sam@bonnekamp.net>
2022-08-28 03:19:25 +0800

    removed unecessary comments and newlines

--- Lily/milestone2/main.c
+++ Lily/milestone2/main.c
@@ -33,8 +33,6 @@ int main(int argc, char* argv[]){
     
     if(strchr(letters, (int)words[i]) == NULL){ //checks if letter already in var letters, if not, add letter
       char stringify[2] = "\0"; //creates {\0, \0} buffer
-
-
       stringify[0] = words[i]; //makes first char of stringify the char, so char becomes null terminated string to be used with strcat
       strcat(letters, stringify); //append letter to letters variable
     }
@@ -42,8 +40,7 @@ int main(int argc, char* argv[]){
 
 
   char forFirstLetters[100]; //the worst solution to this lol
-  strcpy(forFirstLetters, problem); //need to run strtok on this string again later so..
-  
+  strcpy(forFirstLetters, problem); //need to run strtok on this string again later so..  
   char* token = strtok(forFirstLetters, " ");
 
   while(token != NULL){
@@ -53,12 +50,7 @@ int main(int argc, char* argv[]){
     strcat(firstLetter, stringify);
     token = strtok(NULL, " ");
   }
-
-  printf("total letters: %s\n", letters);
-
-
-  int* assignment = assignments(letters, firstLetter);
-  
+  int* assignment = assignments(letters, firstLetter);  
   char temp[100];
   strcpy(temp, problem);
   int test = check_equation(assignment, temp, letters);
@@ -71,43 +63,24 @@ int main(int argc, char* argv[]){
     test = check_equation(assignment, temp, letters);
     funk++;
   }
-
   for (int i = 0; i < strlen(letters); i++){
     printf("%c : %i\n", letters[i], assignment[i]);
-  }
-  
+  }  
 }
 
 
-int* assignments(char* letters, char* firstLetter){
-
-  //static int assign[10] = {[0 ... 9] = 40}; //stores number assignments, initis all values to 40 to avoid random memory noise to interfere with the number selection process, only works with GNU I think, if not on gnu use this instead:
-  //static int assign[10] = {40,40,40,40,40,40,40,40,40,40};
+int* assignments(char* letters, char* firstLetter){  
   int* assign = (int*)malloc(10);
   for (int i = 0; i <= sizeof(assign); i++){
     assign[i] = 41;
-  }
-
-  int hardCode[8] = {9, 5, 6, 7, 1, 0, 8, 2};
-  
-  for (int i = 0; i<strlen(letters); i++){
-    //printf("letter working on: %c\n", letters[i]);
-    //    printf("assign current is: ");
-    //for (int j = 0; j < strlen(letters); j++){
-    //printf("%i", assign[j]);
-    //}
-    //printf("\n");
-    
+  }  
+  for (int i = 0; i<strlen(letters); i++){    
     int r = rand()%10;
     while(check_exists(r, assign, strlen(letters)) == 1 ||(strchr(firstLetter, letters[i])!=NULL && r ==0)){
-      //printf("Stuck at: %c, trying: %d\n", letters[i], r);
       r = rand()%10;
     }
-
     assign[i] = r;
-    //assign[i] = hardCode[i];
   }
-
   return assign;
 }
 
@@ -122,7 +95,6 @@ int check_FirstLetter(char* firstLetters, char letter){
 
 int check_exists(int r, int* assign, size_t len){
   int exists = 0;
-  
   for (int j = 0; j < len-1; j++){
       if(r == assign[j]){
 	exists = 1;
@@ -134,14 +106,7 @@ int check_exists(int r, int* assign, size_t len){
 int check_equation(int* assignment, char* problem, char* letters){
     int left_total = 0;
     int right_total = 0;
-    char answer[50];
-
-    //printf("assign: ");
-    //for (int i = 0; i < strlen(letters); i++){
-    //printf("%i", assignment[i]);	
-    //}
-    //printf("\n");
-    
+    char answer[50]; 
     char* token = strtok(problem, " ");
 
     while(token!=NULL){
@@ -155,26 +120,18 @@ int check_equation(int* assignment, char* problem, char* letters){
       
       for (int i = 0; i < strlen(token); i++){
 	char* e = strchr(letters, token[i]);
-	int index = (int) (e-letters);
-	
+	int index = (int) (e-letters);	
         wordNumber = wordNumber * 10 + assignment[index];
       }
-      //printf("wordNumber = %i\n", wordNumber);
       left_total += wordNumber;
       token = next_token;
     }
-
-    //right side (last word)of equation
     for (int i = 0; i < strlen(answer)-1; i++){
       char* e = strchr(letters, answer[i]);
       int index = (int) (e-letters);
 	
       right_total = right_total * 10 + assignment[index];
-    }
-    
-    //printf("leftTotal = %i\n", left_total);
-    //printf("rightTotal = %i\n", right_total);
-
+    }    
     if(left_total == right_total){
       return 1;
     }else {
